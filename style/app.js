a8d10b2b373c749eae490c13e09fac70bee3e0cd
altin thaqi <ltin.thaqi.520@gmail.com>
2020-06-25 12:28:15 +0200

    Implemented third region, freljord

---
+++ style/app.js
@@ -0,0 +1,11 @@
+// function cardincrease(){
+//     var cardelement = document.getElementById("card");
+//     cardelement.style.height = "400px";
+//     cardelement.style.width = "300px";
+// }
+// function carddecrease(){
+//     var cardelement = document.getElementById("card");
+//     cardelement.style.width = "100px";
+//     cardelement.style.width = "500px";
+// }
+
