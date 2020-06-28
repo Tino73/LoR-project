2b652a559fd5ff2cd5e6de3278e2306410427c45
altin thaqi <ltin.thaqi.520@gmail.com>
2020-06-28 15:27:46 +0200

    Updated sidebar to fully responsive

--- style/app.js
+++ style/app.js
@@ -8,4 +8,23 @@
 //     cardelement.style.width = "100px";
 //     cardelement.style.width = "500px";
 // }
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } 
+    else {
+      x.className = "topnav";
+    }
+}
 
+
+function myFunction() {
+    var x = document.getElementById("mySidebar");
+    if (x.className === "sidebar") {
+      x.className += " responsive";
+    } 
+    else {
+      x.className = "sidebar";
+    }
+}
