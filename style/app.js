d828d610ea1bab25283833c96449e9ee7d5de676
altin thaqi <ltin.thaqi.520@gmail.com>
2020-06-29 23:13:11 +0200

    Implemented Home Page

--- style/app.js
+++ style/app.js
@@ -8,23 +8,50 @@
 //     cardelement.style.width = "100px";
 //     cardelement.style.width = "500px";
 // }
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } 
-    else {
-      x.className = "topnav";
-    }
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//       x.className += " responsive";
+//     } 
+//     else {
+//       x.className = "topnav";
+//     }
+// }
+
+
+// function myFunction() {
+//     var x = document.getElementById("mySidebar");
+//     if (x.className === "sidebar") {
+//       x.className += " responsive";
+//     } 
+//     else {
+//       x.className = "sidebar";
+//     }
+// }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-function myFunction() {
-    var x = document.getElementById("mySidebar");
-    if (x.className === "sidebar") {
-      x.className += " responsive";
-    } 
-    else {
-      x.className = "sidebar";
-    }
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
